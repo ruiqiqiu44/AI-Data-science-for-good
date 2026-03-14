0c602f28af5804d9b30ae87695df2d8b0fa7ea3a
cryolins <hepeilin07@gmail.com>
2026-03-14 12:27:01 -0400

    audio api setup + cors configs

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from tts import text_to_speech
 
 app = FastAPI()
+FRONTEND_URL = "http://localhost:5173"
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
